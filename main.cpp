11f30d1bc8e150d13f0ea2ced64184c3a5c2819a
Sergey Krupov <sergey.krupov@gmail.com>
2016-09-20 15:57:18 +0500

    1

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ namespace naive
     struct FunctorInvokeHelper
     {
         template <typename FUNC, typename ...ARGS>
-        static auto invoke(const FUNC& func, ARGS&& ...args) -> decltype(func(std::forward<ARGS>(args)...))
+        static auto invoke(FUNC& func, ARGS&& ...args) -> void /* decltype(func(std::forward<ARGS>(args)...))*/
         {
             return func(std::forward<ARGS>(args)...);
         }
@@ -51,11 +51,47 @@ namespace naive
     struct MemberInvokeHelper
     {
         template <typename FUNC, typename OBJ, typename ...ARGS>
-        static auto invoke(const FUNC& fn, OBJ&& obj, ARGS&& ...args) -> decltype((obj.*fn)(std::forward<ARGS>(args)...))
+        static auto invoke(FUNC& fn, OBJ&& obj, ARGS&& ...args) -> decltype((obj.*fn)(std::forward<ARGS>(args)...))
         {
             return (obj.*fn)(std::forward<ARGS>(args)...);
         }
     };
+
+    //-----------------------------------------------------------------------
+
+    template <size_t INDEX, typename T, typename ...ARGS>
+    struct TakeValue : public TakeValue<INDEX - 1, ARGS...>
+    {
+        using superclass_t = TakeValue<INDEX - 1, ARGS...>;
+        using result_t = typename superclass_t::result_t;
+
+        static result_t take(T&& t, ARGS&& ...other)
+        {
+            return superclass_t::take(std::forward<ARGS>(other)...);
+        }
+
+    };
+
+    template <typename T, typename ...ARGS>
+    struct TakeValue<0, T, ARGS...>
+    {
+        using result_t = T;
+
+        static T take(T&& v, ARGS&& ...other)
+        {
+            return std::forward<T>(v);
+        }
+    };
+
+    //-----------------------------------------------------------------------
+
+    template <typename T>
+    struct PlaceHolderIndex
+    {};
+
+    template <size_t INDEX>
+    struct PlaceHolderIndex<std::placeholders::__ph<INDEX>> : public std::integral_constant<size_t, INDEX>
+    {};
 
     //-----------------------------------------------------------------------
 
@@ -65,7 +101,7 @@ namespace naive
         template <typename DUMMY, typename ...PHS>
         static auto take(const DUMMY&, PHS&& ...phs) -> typename ArgAtIndex<INDEX, PHS...>::type
         {
-            return std::get<INDEX>(std::make_tuple(std::forward<PHS...>(phs...)));
+            return TakeValue<INDEX, PHS...>::take(std::forward<PHS>(phs)...);
         }
     };
 
@@ -78,14 +114,14 @@ namespace naive
         template <typename ...DUMMY>
         static auto take(args_holder_t& holder, DUMMY&& ...) -> result_t
         {
-            static_assert(0);
             using arg_t = Argument<INDEX, result_t>;
             return static_cast<arg_t *>(&holder)->value;
         }
     };
 
     template <typename T, size_t INDEX, typename ...ARGS>
-    struct Take : TakePlaceholder<INDEX> //public std::conditional<0, TakePlaceholder<INDEX>, TakeArgument<INDEX, ARGS...>>::type
+    struct Take : public std::conditional<std::is_placeholder<std::remove_reference_t<T>>::value,
+            TakePlaceholder<PlaceHolderIndex<std::remove_reference_t<T>>::value - 1>, TakeArgument<INDEX, ARGS...>>::type
     {
     };
 
@@ -109,8 +145,7 @@ namespace naive
         template <size_t ...ARGIDX, typename ...PHS>
         auto invoke_helper(std::index_sequence<ARGIDX...>, PHS&& ...phs) -> void
         {
-            invoker_t::invoke(m_func,
-                              Take<typename ArgAtIndex<ARGIDX, ARGS...>::type, ARGIDX, ARGS...>::take(m_argsHolder, std::forward<PHS>(phs)...)...);
+            invoker_t::invoke(m_func, Take<typename ArgAtIndex<ARGIDX, ARGS...>::type, ARGIDX, ARGS...>::take(m_argsHolder, std::forward<PHS>(phs)...)...);
         };
 
     private:
@@ -134,14 +169,13 @@ int main()
 //    auto bar = naive::bind(foo, 42, 'b', "blah-blah-blah");
 //    bar(1234);
 
-    auto foo = [] (std::string v) {
-        std::cout << "xxx v=" << v << " xxx" << std::endl;
+    auto foo = [] (std::string a, std::string b, std::string c) {
+        std::cout << "a=" << a << ", b=" << b << ", c=" << c << std::endl;
     };
-    auto baz = naive::bind(foo, std::placeholders::_1);
+
+    auto baz = naive::bind(foo, std::placeholders::_1, std::placeholders::_1, std::placeholders::_1);
     baz("zzzz");
 
-
-    std::placeholders::_1;
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
